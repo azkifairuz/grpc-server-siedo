840a8b98865902f941e106cbdcee22603cd3dcc8
azkia ajmal fairuz <azkiajmal@gmail.com>
2024-07-12 10:02:26 +0700

    feat: service presensi online

--- apps/dosen/src/presensi/presensi.service.ts
+++ apps/dosen/src/presensi/presensi.service.ts
@@ -89,5 +89,76 @@ export class PresensiService {
     }
   }
 
+  async presensiOnline(account: Account): Promise<BaseResponse> {
+    try {
+      const dosen = await this.prismaService.dosenAccount.findFirst({
+        where: {
+          account_id: account.uuid,
+        },
+        include: {
+          dosen: true,
+        },
+      });
+      const date = new Date();
+      const currentDate = new Date().toISOString().split('T')[0];
+
+      const today = date.toLocaleDateString('id-ID', { weekday: 'long' });
+      const isAlreadyIzinToday = await this.prismaService.izin.findFirst({
+        where: {
+          tanggal: currentDate,
+        },
+      });
+      if (isAlreadyIzinToday) {
+        return {
+          statusCode: 400,
+          message: 'presensi failed: dosen already izin today',
+        };
+      }
+      const isAlreadyCheckIn = await this.prismaService.riwayatMasuk.findFirst({
+        where: {
+          AND: [
+            {
+              tanggal: currentDate,
+            },
+            {
+              nidn: dosen.nidn,
+            },
+          ],
+        },
+        orderBy: {
+          jam: 'desc',
+        },
+      });
+
+      if (isAlreadyCheckIn != null) {
+        if (isAlreadyCheckIn.kegiatan == 'masuk') {
+          return {
+            statusCode: HttpStatus.BAD_REQUEST,
+            message: 'presensi failed: dosen already presensi',
+          };
+        }
+      }
+      await this.prismaService.riwayatMasuk.create({
+        data: {
+          hari: today.toString(),
+          jam: date.toLocaleTimeString('id-ID'),
+          tanggal: `${currentDate}`,
+          tipe: 'online',
+          nidn: dosen.nidn,
+          kegiatan: 'masuk',
+        },
+      });
+      return {
+        statusCode: HttpStatus.OK,
+        message: 'presensi success',
+      };
+    } catch (error) {
+      return {
+        statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+        message: `error: ${error}`,
+      };
+    }
+  }
+
  
 }
