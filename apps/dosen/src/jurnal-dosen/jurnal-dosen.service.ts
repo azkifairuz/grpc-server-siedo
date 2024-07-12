76241cf644a94d0f5f20f51c9b5e06a2c844180d
azkia ajmal fairuz <azkiajmal@gmail.com>
2024-07-12 22:32:07 +0700

    feat: service get jurnal by id

--- apps/dosen/src/jurnal-dosen/jurnal-dosen.service.ts
+++ apps/dosen/src/jurnal-dosen/jurnal-dosen.service.ts
@@ -2,6 +2,7 @@ import { HttpStatus, Injectable } from '@nestjs/common';
 import { Account } from '@prisma/client';
 import { PrismaService } from 'apps/dosen/common/prisma.service';
 import {
+  GetJurnalByIdResponse,
   GetListJurnalResponse,
   JurnalResponse,
   PaginationData,
@@ -84,6 +85,61 @@ export class JurnalDosenService {
         data: [],
         statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
         message: `error: ${error}`,
+      };
+    }
+  }
+
+  async getJurnalById(
+    account: Account,
+    jurnalId: number,
+  ): Promise<GetJurnalByIdResponse> {
+    try {
+      const dosen = await this.prismaService.dosenAccount.findFirst({
+        where: {
+          account_id: account.uuid,
+        },
+        include: {
+          dosen: {},
+        },
+      });
+
+      const semesterActive = await this.prismaService.semesterAktif.findFirst({
+        where: {
+          status: 'active',
+        },
+      });
+
+      const jurnal = await this.prismaService.jurnal.findFirst({
+        where: {
+          AND: [{ id: jurnalId }],
+        },
+      });
+
+      const jurnalResponse: JurnalResponse = {
+        id: dosen.id,
+        nidn: dosen.nidn,
+        judulArtikel: jurnal.judul_artikel,
+        namaJurnal: jurnal.nama_jurnal,
+        volume: jurnal.volume,
+        nomor: jurnal.nomor,
+        tanggalTerbit: jurnal.tanggal_terbit,
+        issn: jurnal.ISSN,
+        semesterAktif: semesterActive.id,
+        halaman: jurnal.halaman,
+        penerbitPenyelanggara: jurnal.penerbit_penyelanggara,
+        tautanLamanJurnal: jurnal.tatuan_laman_jurnal,
+        uploadDocument: jurnal.upload_document,
+      };
+
+      return {
+        data: jurnalResponse,
+        statusCode: HttpStatus.OK,
+        message: 'success jurnal',
+      };
+    } catch (error) {
+      return {
+        statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+        message: `failed: ${error}`,
       };
     }
   }
