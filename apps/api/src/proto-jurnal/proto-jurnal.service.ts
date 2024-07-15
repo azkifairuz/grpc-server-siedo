e38feb5c4a4faffec711ea67abd0d8ed70cdcbc8
azkia ajmal fairuz <azkiajmal@gmail.com>
2024-07-16 01:04:22 +0700

    grcp clien update and create jurnal

--- apps/api/src/proto-jurnal/proto-jurnal.service.ts
+++ apps/api/src/proto-jurnal/proto-jurnal.service.ts
@@ -2,10 +2,13 @@ import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
 import { ClientGrpc } from '@nestjs/microservices';
 import { Account } from '@prisma/client';
 import {
+  CreateJurnalRequest,
   GetJurnalByIdRequest,
   GetListJurnalRequest,
   JURNAL_SERVICE_NAME,
+  JurnalRequest,
   JurnalServiceClient,
+  UpdateJurnalRequest,
 } from 'proto/jurnal';
 
 @Injectable()
@@ -26,5 +29,33 @@ export class ProtoJurnalService implements OnModuleInit {
   getBydId(account: Account, jurnalId: number = 1) {
     const getByIdRequest: GetJurnalByIdRequest = { account, jurnalId };
     return this.jurnalClientService.getJurnalById(getByIdRequest);
+  }
+
+  updateJurnal(
+    account: Account,
+    jurnalId: number = 1,
+    jurnalRequest: JurnalRequest,
+    document: Uint8Array,
+  ) {
+    const updateJurnalRequest: UpdateJurnalRequest = {
+      account,
+      jurnalId,
+      jurnalRequest,
+      document,
+    };
+    return this.jurnalClientService.updateJurnal(updateJurnalRequest);
+  }
+
+  createJurnal(
+    account: Account,
+    jurnalRequest: JurnalRequest,
+    document: Uint8Array,
+  ) {
+    const createJurnalRequest: CreateJurnalRequest = {
+      account,
+      document,
+      jurnalRequest,
+    };
+    return this.jurnalClientService.createJurnal(createJurnalRequest);
   }
 }
